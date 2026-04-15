ea8259b8711e87fa82f9c98b765707d168411b0f
Tito <manri.carazo@gmail.com>
2026-04-15 01:19:50 -0600

    feat: add footer desk and mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 
 import "./globals.css";
+import Footer from "../components/common/(Footer)/Footer"; 
 import Navbar from "../components/common/Navbar";
+
 
 const myCustomFont = localFont({
   src: "../assets/fonts/Bernard_MT_Condensed_Regular.ttf",
@@ -24,8 +26,11 @@ export default function RootLayout({
       <body className={`${myCustomFont.variable} ${myCustomFont.className}`}>
         <Navbar />
 
-        <main>{children}</main>
+        <main style={{ minHeight: "calc(100vh - var(--nav-height) - 300px)" }}>
+          {children}
+        </main>
 
+        <Footer /> 
       </body>
     </html>
   );
