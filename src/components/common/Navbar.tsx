3ce86790ddb69872d3641073616b0d7df997b9f9
Tito <manri.carazo@gmail.com>
2026-04-15 00:35:28 -0600

    fix: mobile - resize imgs - minor changes

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,27 +1,38 @@
 "use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react"; 
 
+import iconMenu from "@/assets/icons//Hamburger-Menu.webp";
 import logoHikisa from "@/assets/imgs/CarnesHikisa_RB.webp";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false); // Estado para mobile
 
   return (
     <nav className={styles.navbar}>
-      <div className={styles["navbar-links"]}>
+      {/* Botón Hamburguesa - Solo visible en mobile */}
+      <button className={styles.hamburger} onClick={() => setIsOpen(!isOpen)}>
+        <Image src={iconMenu} alt="Menu" width={30} height={30} />
+      </button>
+
+      <div className={`${styles["navbar-links"]} ${isOpen ? styles.show : ""}`}>
         <Link
           href="/catalogo"
           className={pathname === "/catalogo" ? styles.active : ""}
+          onClick={() => setIsOpen(false)}
         >
           Catálogo
         </Link>
         <Link
           href="/contactos"
           className={pathname === "/contactos" ? styles.active : ""}
+          onClick={() => setIsOpen(false)}
         >
           Contactos
         </Link>
@@ -32,9 +43,9 @@ const Navbar = () => {
           <Image
             src={logoHikisa}
             alt="Carnes Hikisa"
-            width={250}
-            height={100}
-            style={{ height: "auto", width: "250px" }}
+            width={200} 
+            height={80}
+            className={styles.logoImg}
             priority
           />
         </Link>
@@ -43,7 +54,7 @@ const Navbar = () => {
       <div className={styles["navbar-search"]}>
         <div className={styles["search-container"]}>
           <span className={styles["search-icon"]}>🔍</span>
-          <input type="text" placeholder="Buscar producto..." />
+          <input type="text" placeholder="Buscar..." />
         </div>
       </div>
     </nav>
