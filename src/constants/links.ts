d97d31b5f0fcdbde6b2b286355ab9325aaec9fb1
TitoManri <manri.carazo@gmail.com>
2026-06-10 12:58:59 -0600

    fix: update wa message

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,6 +1,6 @@
 // Codificación del mensaje para la URL
 const WHATSAPP_MESSAGE = encodeURIComponent(
-  "¡Buenas, deseo consultar la lista de precios y cortes disponibles.",
+  "¡Hola! 😊 Quisiera consultar los precios, promociones y productos disponibles. ¡Gracias!",
 );
 
 export const SOCIAL_LINKS = {
@@ -8,5 +8,5 @@ export const SOCIAL_LINKS = {
   instagram:
     "https://www.instagram.com/carnes_hikisa?fbclid=IwY2xjawRMHi5leHRuA2FlbQIxMABicmlkETE4ekRHaW9uaVhxNkZEWktzc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHhmAlIvr2sud8VijMHWk6EPOm5CQu8dHL_owwfyj45klDWGxSvKN9l5-qoyy_aem_5eMV1pYQrtSXElK-ci0kGA",
   tiktok: "https://www.tiktok.com/@carnes.hikisa",
-  whatsapp: `https://wa.me/50660124222?text=${WHATSAPP_MESSAGE}`,
+  whatsapp: `https://api.whatsapp.com/send?phone=50660124222&text=${WHATSAPP_MESSAGE}`,
 };
